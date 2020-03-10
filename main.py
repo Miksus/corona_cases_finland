4a115071c0fb0dd969c3b12863c335f4de96695c
Mikael Koli <koli.mikael@gmail.com>
2020-03-10 07:47:01 +0200

    add: HTML outputting

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
+import datetime
 
 import pandas as pd
 
 from bokeh.models import Plot, graphs, ColumnDataSource, Range1d, Circle, HoverTool, BoxZoomTool, ResetTool, WheelZoomTool
-from bokeh.io import show as bokeh_show
+from bokeh.io import show
 
 import networkx as nx
+from bokeh.io import output_file
 
+# Output for the graph
+output_file(fr"graphs/{datetime.date.today()}_infections.html")
+
+# Hard coded mappings
 country_mapping = {"FIN": "blue", "CHN": "red", "ITA": "green"}
 
 # Get data from HS API
@@ -40,6 +46,7 @@ plot.title.text = f"Corona cases Finland"
 tools = [HoverTool(tooltips=[("id", "@id"), ("loc", "@location"), ("date", "@date"), ("origin", "@origin")]), WheelZoomTool(), ResetTool(), BoxZoomTool()]
 plot.add_tools(*tools)
 
+# Set Networkx graph to Bokeh
 graph_renderer = graphs.from_networkx(G, **kwds_network)
 graph_renderer.node_renderer.data_source.data = ColumnDataSource(data=plot_data).data
 
@@ -48,4 +55,4 @@ graph_renderer.node_renderer.glyph = Circle(size=10, fill_color="colors")
 plot.renderers.append(graph_renderer)
 
 # Show the plot
-bokeh_show(plot)
+show(plot)
